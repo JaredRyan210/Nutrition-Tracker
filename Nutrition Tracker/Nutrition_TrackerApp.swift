936c0377274ff0c37d5806969efa945f011a1e43
JRyan109 <124639959+JRyan109@users.noreply.github.com>
2024-07-18 16:22:12 -0700

    Initial Project Creation

---
+++ Nutrition Tracker/Nutrition_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nutrition_TrackerApp.swift
+//  Nutrition Tracker
+//
+//  Created by Jared Ryan on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Nutrition_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
